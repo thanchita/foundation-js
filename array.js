120ef31524e700d0f6036d0b488f958f38b72bc5
Aekkrit Kuntipalo <aekkritkuntipalo@Aekkrits-MacBook-Air.local>
2023-09-12 12:18:18 +0700

    add basic javascript file

---
+++ array.js
@@ -0,0 +1,5 @@
+const myFavoriteFood = ["Noodle", "Padthai", "Hotdog", "Soup"];
+console.log(myFavoriteFood.indexOf("Hotdog"));
+myFavoriteFood.push("Orange", "Mango", "Grape");
+console.log(myFavoriteFood);
+console.log(myFavoriteFood.includes("Hotdog"));
